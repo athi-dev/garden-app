6af75b9d57bfb9a810e3d5794f45c634887385b5
athi-dev <athikani@mail.com>
2025-08-26 15:44:28 +0200

    Add files via upload

---
+++ git/garden_advice.js
@@ -0,0 +1,15 @@
+// Garden Advice App
+// TODO: Refactor hardcoded advice into functions
+// TODO: Add documentation for clarity
+
+const month = "August"; 
+
+if (month === "March") {
+  console.log("Time to start planting cool-season crops.");
+} else if (month === "July") {
+  console.log("Water frequently and mulch to retain moisture.");
+} else if (month === "October") {
+  console.log("Prepare your soil for winter.");
+} else {
+  console.log("General gardening advice: Keep your plants healthy!");
+}
